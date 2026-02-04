45803b46d02944eedb61990542805d79df868a58
TANISHK RAJ <152382165+Tanishkraj2005@users.noreply.github.com>
2026-02-04 17:46:06 +0530

    Time: 1 ms (24.75%), Space: 10.4 MB (97.11%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        // For first rectangle 
+        int area1 = (ax2-ax1) * (ay2-ay1); // length * breadth
+
+        // for second rectangle
+        int area2 = (bx2-bx1) * (by2-by1);
+
+        // calculate the intersecting area
+        int bottomLeftX = max(ax1, bx1);
+        int bottomLeftY = max(ay1, by1);
+
+        int topRightX = min(ax2, bx2);
+        int topRightY = min(ay2, by2);
+
+        int width = topRightX - bottomLeftX;
+        int height = topRightY - bottomLeftY;
+        int intersectingArea = height * width;
+
+        if(width <= 0 || height <= 0)
+            intersectingArea = 0;
+
+        return area1 + area2 - intersectingArea;
+    }
+};
