b88640bbf3160d6b8beba57f11298fd543705efc
Tanishkraj2005 <rajtanishk475@gmail.com>
2026-02-08 22:10:49 +0530

    Did some changes

--- Coding_Cpp/Binary Tree/Level_order_traversal_from_left_to_right.cpp
+++ Coding_Cpp/Binary Tree/Level_order_traversal_from_left_to_right.cpp
@@ -1,8 +1,7 @@
 #include<iostream>
-#include<climits>
 using namespace std;
 class Node{
-public:
+public: 
     int val;
     Node* left;
     Node* right;
@@ -12,11 +11,17 @@ public:
         this->right=NULL;
     }
 };
+void display(Node* root){
+    if(root==NULL) return;
+    cout<<root->val<<" ";
+    display(root->left);
+    display(root->right);
+}
 int level(Node* root){
     if(root==NULL) return 0;
     return 1+max(level(root->left),level(root->right));
 }
-void nthLevel(Node* root, int curr,int level){
+void nthLevel(Node* root,int curr,int level){
     if(root==NULL) return;
     if(curr==level) cout<<root->val<<" ";
     nthLevel(root->left,curr+1,level);
@@ -25,32 +30,27 @@ void nthLevel(Node* root, int curr,int level){
 void levelOrder(Node* root){
     int n=level(root);
     for(int i=0;i<=n;i++){
-        nthLevel(root,1,i);
+        nthLevel(root,0,i);
         cout<<endl;
     }
 }
 int main(){
-    Node* a = new Node(1);  
+    Node* a = new Node(1);  // root
     Node* b = new Node(2);
     Node* c = new Node(3);
     Node* d = new Node(4);
     Node* e = new Node(5);
     Node* f = new Node(6);
     Node* g = new Node(7);
-    Node* h = new Node(8);
-    Node* i = new Node(9);
-    Node* j = new Node(10);
-    Node* k = new Node(11);
     a->left = b;
     a->right = c;
     b->left = d;
     b->right = e;
     c->left = f;
     c->right = g;
-    d->left=h;
-    e->left=i;
-    e->right=j;
-    g->left=k;
-    nthLevel(a,1,3);
-    
+    display(a);
+    cout<<endl;
+    levelOrder(a);
 }
+
+
