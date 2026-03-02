258db81254639abacab2ba0e632bc43c953f1b3d
TANISHK RAJ <152382165+Tanishkraj2005@users.noreply.github.com>
2026-03-02 18:41:27 +0530

    Time: 0 ms (100%), Space: 12.7 MB (32.22%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,10 +5,15 @@ public:
         if(n==1) return 0;
         if(arr[0]>arr[1]) return 0;
         if(arr[n-1]>arr[n-2]) return n-1;
-        for(int i=1;i<n-1;i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                return i;
+        int low = 1;
+        int high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) return mid;
+            if(arr[mid]<arr[mid+1]){
+                low = mid+1;
             }
+            else high = mid-1;
         }
         return -1;
     }
