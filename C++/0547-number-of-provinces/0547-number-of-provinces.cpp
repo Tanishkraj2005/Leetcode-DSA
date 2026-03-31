e10da4361176e256b7526d6a5e59989b37790551
TANISHK RAJ <152382165+Tanishkraj2005@users.noreply.github.com>
2026-03-31 22:28:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- C++/0547-number-of-provinces/0547-number-of-provinces.cpp
+++ C++/0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,38 +1,38 @@
 class Solution {
 private:
-    void dfs(int node, vector<vector<int>> &adjList, vector<int> &vis){
+    void dfs(int node, vector<int> adjList[], int vis[]){
         vis[node] = 1;
-        for(auto it : adjList[node]){
+        for(auto it: adjList[node]){
             if(!vis[it]){
                 dfs(it, adjList, vis);
             }
         }
     }
-
 public:
     int findCircleNum(vector<vector<int>>& adj) {
         int n = adj.size();
-        vector<vector<int>> adjList(n);
+        vector<int> adjList[n];
 
-        // Convert adjacency matrix to adjacency list
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        // to change adjacency matrix to list
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 if(adj[i][j] == 1 && i != j){
                     adjList[i].push_back(j);
+                    adjList[j].push_back(i);
                 }
             }
         }
 
-        vector<int> vis(n, 0);
-        int c = 0;
+        int vis[n];
+        memset(vis, 0, sizeof(vis));  HERE
 
-        for(int i = 0; i < n; i++){
+        int c = 0;
+        for(int i=0;i<n;i++){
             if(!vis[i]){
                 c++;
                 dfs(i, adjList, vis);
             }
         }
-
         return c;
     }
 };
